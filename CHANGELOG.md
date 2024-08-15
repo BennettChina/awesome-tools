d9d5ee34613d9386213b9a08ed9f4eb9113a9152
bennett <bennett_asia@outlook.com>
2024-08-15 16:17:58 +0800

    fix: 支持指令更新

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.1.1
+
+- fix: 支持指令更新
+
 ## v1.1.0
 
 - feature: 添加设置Bot头像功能（目前仅支持 [onebots](https://github.com/lc-cn/onebots) 客户端）
--- init.ts
+++ init.ts
@@ -10,6 +10,11 @@ export let config: typeof initConfig;
 export default definePlugin( {
 	name: "Awesome Tools",
 	cfgList,
+	repo: {
+		owner: "BennettChina",
+		repoName: "awesome-tools",
+		ref: "master"
+	},
 	async mounted( params ) {
 		config = params.configRegister( "main", initConfig );
 		params.setAlias( [ "工具集" ] );
